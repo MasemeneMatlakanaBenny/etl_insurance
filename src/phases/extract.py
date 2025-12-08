8fc1625368c669f83c204a7fe4bfb0a0b942be9a
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-12-08 21:27:25 +0200

    add  extraction code

--- src/phases/extract.py
+++ src/phases/extract.py
@@ -1,1 +1,23 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+
+## load the dotenv first:
+load_dotenv()
+
+## get db name and passwords from env file:
+password=os.getenv("PASSWORD")
+db_name=os.getenv("DB_NAME")
+user=os.getenv("USER")
+
+engine=create_engine(f"mysql+pymysql://{user}:{password}@127.0.0.1:3306/{db_name}")
+
+## then access the db:
+query="SELECT * FROM insurance"
+
+df=pd.read_sql_squery(query,engine)
+
+df.to_csv("data/extracted_df.csv",index=False)
 
