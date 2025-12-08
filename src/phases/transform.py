2a17d7931fc0622d29edb9d03c6ba8fdd8e99c40
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-12-08 21:29:59 +0200

    add transformation code in phases folder

--- src/phases/transform.py
+++ src/phases/transform.py
@@ -1,3 +1,20 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+df=pd.read_csv("data/extracted_df.csv")
+
+unique_ids=np.arange(1,len(df)+1)
+current_time=datetime.now()
+
+
+df['unique_id']=unique_ids
+df['time']=current_time
+
+## save the dataset to the data folder in a csv format:
+df.to_csv("data/transformed_df.csv",index=False)
+
+
 
 
 
